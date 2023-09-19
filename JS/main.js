1dc15abb88c8bfa675e8bc1c6c19ba1d1b2a5601
FedericaMammone <federicakr97@gmail.com>
2023-09-19 22:02:44 +0200

    corretto

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,11 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
             error: false,
             newTask: '',
-            text:'',
+            text: '',
             tasks: [
                 {
                     text: 'Fare i compiti',
@@ -23,37 +23,37 @@ createApp({
                     text: 'Dare da mangiare ai gatti',
                     done: false
                 }
-                
+
             ]
         }
     },
     methods: {
-        aggiungiTask(){
+        aggiungiTask() {
             console.log(this.newTask);
 
-            if(this.newTask !== '' && this.newTask.length >= 5){
-                const oggettoTask = 
+            if (this.newTask !== '' && this.newTask.length >= 5) {
+                const oggettoTask =
                 {
                     text: this.newTask,
                     done: false
                 }
                 this.tasks.unshift(oggettoTask);
                 this.error = false;
-            }else {
+            } else {
                 this.error = true;
             }
-            
+
             this.newTask = '';
         },
-        rimuoviTask(indice){
+        rimuoviTask(indice) {
             this.tasks.splice(indice, 1);
         },
-        invertDone(index){
+        invertDone(index) {
             console.log(index);
 
-            if(index.done === true){
+            if (index.done === true) {
                 index.done = false;
-            }else{
+            } else {
                 index.done = true;
 
             }
